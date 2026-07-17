286c0b0d445b2f9758a0945894ead49de30b9833
Isa Yan <isabel.yyj.yan@gmail.com>
2026-07-18 03:26:59 +0800

    something's going on. i lowk forgot that red can only correlate to diamond and heart so i had to redo it and get red of bool is_red just cuz it wouldn't make sense. tried to refigure out my shit and what's going on with the card traits and i think i got it now. i also added the jokers to the card_value vector so that they can be used in the game.

--- guandan.cpp
+++ guandan.cpp
@@ -12,58 +12,53 @@ clang++ -std=c++17 -g guandan.cpp -o guandan
 */
 
 int main(){
-    // spade, heart, diamond, club
-    vector<string> suits = {"\u2660","\u2665","\u2666","\u2663"};
-    
-    //colors 
-    bool is_red;
-
     //each card's number and value
-    vector<tuple<string,int>> card_value;
+        vector<tuple<string,int>> card_value;
     
     //numbers 
     //i end up not even understanding my own code at times
-    for (int i=0;i<=25;i++){
-        if (i<=10)
-            card_value.push_back(tuple<string,int>(to_string(i+1),i+1));
-        else if (i==11)
-            card_value.push_back(tuple<string,int>("J",i-1));
-        else if (i==12)
-            card_value.push_back(tuple<string,int>("Q",i-1));
-        else if (i==13)
-            card_value.push_back(tuple<string,int>("K",i-1));
-        else if (i==14)
-            card_value.push_back(tuple<string,int>("A",i-1));
+        for (int i=0;i<=25;i++){
+            if (i<=10)
+                card_value.push_back(tuple<string,int>(to_string(i+1),i+1));
+            else if (i==11)
+                card_value.push_back(tuple<string,int>("J",i-1));
+            else if (i==12)
+                card_value.push_back(tuple<string,int>("Q",i-1));
+            else if (i==13)
+                card_value.push_back(tuple<string,int>("K",i-1));
+            else if (i==14)
+                card_value.push_back(tuple<string,int>("A",i-1));
+            else if (i==15)
+                card_value.push_back(tuple<string,int>("Small Joker",i-1));
+            else if (i==16)
+                card_value.push_back(tuple<string,int>("Big Joker",i-1));
 
-    }
+
+        }
+
+    // suits: order: heart, diamond, club, spade
+        vector<string> suits = {"\u2665","\u2666","\u2663","\u2660"};
+
 
     //card itself
-    tuple<int,string,bool> card_traits;
+        tuple<int,string,string> card_traits;
+            for (int i=0;i<card_value.size();i++){
+                get<0>(card_traits)=get<1>(card_value[i]);
+                
 
-    if(is_red=true){
-        for (int i=0;i<card_value.size();i++){
-            get<0>(card_traits) = i;
-            get<2>(card_traits) = true;
-
-            for (int j=0;j<suits.size();j++){
-                get<1>(card_traits) = suits[j];
+                for (int j=0;j<suits.size();j++){
+                    get<1>(card_traits) = suits[j];
                 }
+            }
+        
+    //test
+        
 
-        }
-    }else if(is_red=false){
-        for (int i=0;i<card_value.size();i++){
-            get<0>(card_traits) = i;
-            get<2>(card_traits) = false;
-
-            for (int j=0;j<suits.size();j++){
-                get<1>(card_traits) = suits[j];
-                }
-
-        }
-    }
+    
 
 
 
 
     return 0;
-}
+    
+    }
