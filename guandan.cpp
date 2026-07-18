a92d78686f43d96dfbb9a656284beb4fc522fc58
Isa Yan <isabel.yyj.yan@gmail.com>
2026-07-18 23:57:30 +0800

    it works sorta now. im lowk over comitting but the more the merrier. basically i just decided to make a vector that is supposed to only contain card_traits. plus in the struct moi added an int for value, string for on screen value, a stirng for suit, and a bool for whether or not it's a trump card :p

--- guandan.cpp
+++ guandan.cpp
@@ -12,47 +12,54 @@ clang++ -std=c++17 -g guandan.cpp -o guandan
 */
 
 int main(){
-    //each card's number and value
-        vector<tuple<string,int>> card_value;
-    
-    //numbers 
-    //i end up not even understanding my own code at times
-        for (int i=0;i<=16;i++){
-            if (i<=10)
-                card_value.push_back(tuple<string,int>(to_string(i+1),i+1));
-            else if (i==11)
-                card_value.push_back(tuple<string,int>("J",i-1));
-            else if (i==12)
-                card_value.push_back(tuple<string,int>("Q",i-1));
-            else if (i==13)
-                card_value.push_back(tuple<string,int>("K",i-1));
-            else if (i==14)
-                card_value.push_back(tuple<string,int>("A",i-1));
+
+    /*
+    twas getting too confusing with tuples so i changed to structs so i would still have some hair by the end o fmy project
+    */
+
+    //setting up shit in here
+    vector<string> suits={"\u2660","\u2665","\u2666","\u2663"};
+
+    //setting up the struct
+    struct card_traits{
+        int value;
+        string on_screen_value;
+        string suit;
+        bool is_trump;
+
+    };
+
+    vector<card_traits> cards;
+
+    //putting shit in the struct
+    //rn i'm trying out putting this within a suit for loop
+    for(int i=0; i<suits.size();i++){
+        for (int i=0; i<14; i++){
+            cards[i].value=i+1;
+            if(i<=10)
+                cards[i].on_screen_value=to_string(i);
+            else if(i==11)
+                cards[i].on_screen_value="J";
+            else if(i==12)
+                cards[i].on_screen_value="Q";
+            else if(i==13)
+                cards[i].on_screen_value="K";
+            else if(i==14)
+                cards[i].on_screen_value="A";
+
+
+            if (cards[i].is_trump==true)
+                cards[i].value=15;
+
 
 
         }
+    }
+    //suit shit here. so i need the cards to fit certain suits but idk how to do that :thonk:
+ 
 
-    // suits: order: heart, diamond, club, spade
-        vector<string> suits = {"\u2665","\u2666","\u2663","\u2660"};
 
 
-    //card itself
-        vector<tuple<int,string,string>> card_traits;
-            for (int i=0;i<card_value.size();i++){
-                card_traits.push_back(tuple(get<1>(card_value[i])));
-
-                for (int j=0;j<suits.size();j++){
-                    get<1>(card_traits[i]) = suits[j];
-                }
-            }
-        
-    //test
-        for (int i=0; i<card_traits.size();i++){
-            cout<<get<0>(card_traits[i]);
-            cout<<get<1>(card_traits[i]);
-        }
-
-    
 
 
 
