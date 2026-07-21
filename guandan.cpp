9c4f4073c526a6719e494d28cf71191ace93ec90
Isa Yan <isabel.yyj.yan@gmail.com>
2026-07-21 17:32:09 +0800

    changed the vector of cards into an array instead cuz it'd be easier to deal with since there's a limited amount of cards :D goddamit i suck at coding ai could do this in 5 minutes i took 3 hours cuz i suck

--- guandan.cpp
+++ guandan.cpp
@@ -29,38 +29,39 @@ int main(){
 
     };
 
-    vector<card_traits> cards;
+    array<card_traits,26> cards;
 
     //putting shit in the struct
     //rn i'm trying out putting this within a suit for loop
-    for(int i=0; i<suits.size();i++){
-        for (int i=0; i<14; i++){
-            cards[i].value=i+1;
-            if(i<=10)
-                cards[i].on_screen_value=to_string(i);
-            else if(i==11)
-                cards[i].on_screen_value="J";
-            else if(i==12)
-                cards[i].on_screen_value="Q";
-            else if(i==13)
-                cards[i].on_screen_value="K";
-            else if(i==14)
-                cards[i].on_screen_value="A";
+    for(int i=0;i<suits.size();i++){
+        cards[i].suit=suits[i];
 
+            for (int i=0; i<14; i++){
+                cards[i].value=i+1;
 
-            if (cards[i].is_trump==true)
-                cards[i].value=15;
+                if(i==0){
+                    cards[0].on_screen_value="A";
+                    cards[0].value=13;
+                }
 
+                else if(i<=9)
+                    cards[i].on_screen_value=to_string(i+1);
+                else if(i==10)
+                    cards[i].on_screen_value="J";
+                else if(i==11)
+                    cards[i].on_screen_value="Q";
+                else if(i==12)
+                    cards[i].on_screen_value="K";
 
+                if (cards[i].is_trump==true)
+                    cards[i].value=14;
 
+                cout<<cards[i].on_screen_value;
+            }
         }
-    }
+
     //suit shit here. so i need the cards to fit certain suits but idk how to do that :thonk:
  
-
-
-
-
 
 
 
