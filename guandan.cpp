bec83a6f8e0e99fc34a80a31b7b3516b7ea3d89f
Isa Yan <isabel.yyj.yan@gmail.com>
2026-07-22 21:09:51 +0800

    YIPPEE THE DUMBASS HAS FINALLY MADE CARD :YAYAYAYAY: I HATE MYSELF AND MY CODING ABILITY I SHOULD JUST VIBECODE THIS SHIT FML

--- guandan.cpp
+++ guandan.cpp
@@ -33,32 +33,34 @@ int main(){
 
     //putting shit in the struct
     //rn i'm trying out putting this within a suit for loop
-    for(int i=0;i<suits.size();i++){
-        cards[i].suit=suits[i];
+    for(int j=0;j<suits.size();j++){
 
-            for (int i=0; i<14; i++){
-                cards[i].value=i+1;
+        for (int i=0; i<13; i++){
+            cards[i].value=i+1;
+            cards[i].suit=suits[j];
 
-                if(i==0){
-                    cards[0].on_screen_value="A";
-                    cards[0].value=13;
-                }
-
-                else if(i<=9)
-                    cards[i].on_screen_value=to_string(i+1);
-                else if(i==10)
-                    cards[i].on_screen_value="J";
-                else if(i==11)
-                    cards[i].on_screen_value="Q";
-                else if(i==12)
-                    cards[i].on_screen_value="K";
-
-                if (cards[i].is_trump==true)
-                    cards[i].value=14;
-
-                cout<<cards[i].on_screen_value;
+            if(i==0){
+                cards[0].on_screen_value="A";
+                cards[0].value=13;
             }
+
+            else if(i<=9)
+                cards[i].on_screen_value=to_string(i+1);
+            else if(i==10)
+                cards[i].on_screen_value="J";
+            else if(i==11)
+                cards[i].on_screen_value="Q";
+            else if(i==12)
+                cards[i].on_screen_value="K";
+
+            if (cards[i].is_trump==true)
+                cards[i].value=14;
+
+            cout<<cards[i].on_screen_value<<cards[i].suit<<"\n";
+            
         }
+    }
+    
 
     //suit shit here. so i need the cards to fit certain suits but idk how to do that :thonk:
  
