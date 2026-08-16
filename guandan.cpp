6ebd346a32688ac1a8ed1bedda7cfc6d3f75f812
Isa Yan <isabel.yyj.yan@gmail.com>
2026-08-17 05:26:42 +0800

    ok so i decided to delete all my code and start again since i took a break and completely lost my train of thought. except for that i didn't really redo anything, this was just for like a mental rest. i got rid of is_trump and the total_cards array since i don't think i need them. i also changed the struct name to card and changed the names of some of the variables to be more descriptive. i also added a suit_legible array so that i can use it to display the suit in a more legible way. i also added a suit_symbol variable to the card struct so that i can use it to display the suit symbol. i also added a one_suit_card array so that i can use it to store the cards for each suit.

--- guandan.cpp
+++ guandan.cpp
@@ -1,9 +1,8 @@
-#include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
-#include <tuple>
-
+#include <string>
+#include <iostream>
+#include <array>
 using namespace std;
 
 /*
@@ -11,58 +10,33 @@ clang++ -std=c++17 -g guandan.cpp -o guandan
 ./guandan
 */
 
-int main(){
+vector<string> suit_value{"\u2660","\u2665","\u2666","\u2663"};
+vector<char> suit_legible{'D','S','H','C'};
+vector<string> on_screen_display{"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
 
-    /*
-    twas getting too confusing with tuples so i changed to structs so i would still have some hair by the end o fmy project
-    */
-
-    //setting up shit in here
-    vector<string> suits={"\u2660","\u2665","\u2666","\u2663"};
-    vector<string> on_screen_values={"1","2","3","4","5","6","7","8","9","10","J","Q","K","A"};
-
-    //setting up the struct
-    struct card_traits{
-        int value;
-        string on_screen_value;
-        bool is_trump=false;
-        string suit;
-
-    };
-
+struct card {
+    string on_screen_value;
+    int value;
+    string suit_symbol;
+    char suit;
     
-    array<array<card_traits,26>,4> total_cards;
-    array<card_traits,26> cards;
+};
 
-    //putting shit in the struct
-    //rn i'm trying out putting this within a suit for loop
+array<array<card,13>,4> one_suit_card;
+
+int main(){
     for(int j=0;j<4;j++){
-        for (int i=0; i<13; i++){
-            cards[i].value=i+1;
-            cards[i].on_screen_value=on_screen_values[i];
-            cards[i].suit=suits[j];
-
-            total_cards.at(j).at(i).value=cards[i].value;
-            total_cards.at(j).at(i).suit=cards[i].suit;
-            total_cards.at(j).at(i).on_screen_value=cards[i].on_screen_value;
-
-            /*for (int k=0;k<4;k++){
-                total_cards[k[i]].suit=suits[j];
-            }*/
-            
+        for(int i=0;i<one_suit_card.size();i++){
+            one_suit_card[j][i].on_screen_value=on_screen_display[i];
+            one_suit_card[j][i].value=i+2;
+            one_suit_card[j][i].suit_symbol=suit_value[j];
+            one_suit_card[j][i].suit=suit_legible[j];
+            cout<<one_suit_card[j][i].suit;
+            cout<<one_suit_card[j][i].on_screen_value;
         }
-    }
-    for (int i=0;i<total_cards.size();i++){
-        for (int j=0;j<108;j++){
-            cout<<total_cards[i][j].on_screen_value<<total_cards[i][j].suit<<"\n";
-        }
-    }
-
-    //suit shit here. so i need the cards to fit certain suits but idk how to do that :thonk:
- 
-
+        
+    }   
 
 
     return 0;
-    
-    }
+}
